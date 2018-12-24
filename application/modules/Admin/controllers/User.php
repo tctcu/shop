c881e1e04448c528c4e9a780ec7b9edecf44ec69
tctcv <xm0563@qq.com>
2018-12-24 14:44:10 +0800

    用户表

--- application/modules/Admin/controllers/User.php
+++ application/modules/Admin/controllers/User.php
@@ -6,7 +6,22 @@ class UserController extends AdminController
 	{
 		parent::init();
 	}
-
+	#用户列表
 	function indexAction(){
+		$condition = array();
+		$page = isset($_REQUEST['page']) ? intval($_REQUEST['page']) : 1;
+		$page_size = 20;
+		$admin_user_model = new UserModel();
+		$show_list = $admin_user_model->getListData($page,$page_size,$condition);
+
+		$this->_view->page = $page;
+		$this->_view->show_list = $show_list;
+		#分页处理
+		$total_num = $admin_user_model->getListCount($condition);
+		$pagination = $this->getPagination($total_num, $page, $page_size);
+		$this->_view->page = $page;
+		$this->_view->pager = new System_Page($this->base_url, $condition, $pagination);
+
+		$this->_layout->meta_title = '用户列表';
 	}
 }
