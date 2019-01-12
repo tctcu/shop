d158299f8ec9059ce5a7e6c2ed20e2d3120a4a1a
tctcv <xm0563@qq.com>
2019-01-12 22:16:05 +0800

    tpwd

--- application/models/TbDetail.php
+++ application/models/TbDetail.php
@@ -60,12 +60,12 @@ class TbDetailModel extends MysqlModel
                 'share_pic' => $info['itempic'] . '_150x150.jpg',
                 'share_url' => 'http://' . $_SERVER['HTTP_HOST'] . '/Web/shareDetail?itemid=' . $info['itemid'] . '&token=',
                 'share_tpwd' => $info['itemshorttitle'] . "\n
-                                  --------\n
-                                【在售价】". $info['itemprice'] ."元\n
-                                【到手价】". $info['itemendprice'] ."元\n
-                                  --------\n
-                                復·制这段描述【tpwd】,\n
-                                咑閞淘♂寳♀即可查看"
+                --------\n
+                【在售价】". $info['itemprice'] ."元\n
+                【到手价】". $info['itemendprice'] ."元\n
+                --------\n
+                復·制这段描述【tpwd】,\n
+                咑閞淘♂寳♀即可查看"
             ]
         ];
     }
