30d971afebe37286a12401287d9223f304d3c71f
tctcv <xm0563@qq.com>
2019-02-21 17:10:34 +0800

    后缀

--- application/models/Common.php
+++ application/models/Common.php
@@ -6,7 +6,7 @@ class CommonModel extends MysqlModel {
     const IMAGE_MIDDLE_SIZE = '?imageView2/1/w/300/h/300';
     const IMAGE_SMALL_SIZE = '?imageView2/1/w/150/h/150';
 
-    const BIND_WE_CHAT = 'bind_wechat.png';//绑定微信授权二维码图
+    const BIND_WE_CHAT = 'bind_wechat.jpg';//绑定微信授权二维码图
 
 
     function __construct(){
