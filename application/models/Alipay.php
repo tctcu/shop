71ce2d6d86267dc966ae4b45482f3ab40df22cd3
sl <xm0563@qq.com>
2019-03-06 16:51:47 +0800

    支付宝测试

--- application/models/Alipay.php
+++ application/models/Alipay.php
@@ -68,7 +68,7 @@ class AlipayModel
 
         $bizContent = array(
             "out_biz_no" => $out_biz_no,
-            "payee_type" => 'ALIPAY_USERID',//'ALIPAY_LOGONID',//收款方账户类型 1、ALIPAY_USERID：支付宝账号对应的支付宝唯一用户号。以2088开头的16位纯数字组成。 2、ALIPAY_LOGONID：支付宝登录号，支持邮箱和手机号格式。
+            "payee_type" => 'ALIPAY_LOGONID',//'ALIPAY_LOGONID',//收款方账户类型 1、ALIPAY_USERID：支付宝账号对应的支付宝唯一用户号。以2088开头的16位纯数字组成。 2、ALIPAY_LOGONID：支付宝登录号，支持邮箱和手机号格式。
             "payee_account" => $payee_account,
             "amount" => $amount,
             "payer_show_name" => '券购',//付款方姓名
