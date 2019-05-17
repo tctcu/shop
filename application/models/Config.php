d3f713ed8f1c65e15ed73eee3ff2bae20cf47fdc
sl <xm0563@qq.com>
2019-05-17 17:00:47 +0800

    test

--- application/models/Config.php
+++ application/models/Config.php
@@ -4,7 +4,7 @@ class ConfigModel{
     const PID = 'mm_234440039_166200410_57891600477';//'mm_116356778_18618211_65740777';
     const RATE = '0.01';//换算佣金比例%
     const REBATE = '0.5';//返利比例
-    const INVITE = '0.1';//返利比例
+    const INVITE = '0.1';//师傅提成返利比例
     const MEMBER = [//对应config
         '234440039' => 1,
         '116356778' => 2,
