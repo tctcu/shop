c618d5fc4e218e2fd1a9a8b593703c8a55592aa9
tctcv <xm0563@qq.com>
2019-06-01 23:23:50 +0800

    返回类型

--- application/controllers/Test.php
+++ application/controllers/Test.php
@@ -275,12 +275,12 @@ echo $url;die;
     }
 
     function success(){
-        echo "<script type='text/javascript'> alert('授权成功'); window.location.href='javascript:native.authTaobaoSuccess()';</script>";
+        echo "<script type='text/javascript'> alert('授权成功'); window.location.href='NativeInterface.authTaobaoSuccess()';</script>";
         exit;
     }
 
     function error($msg){
-        echo "<script type='text/javascript'> alert('授权失败 ".$msg."'); window.location.href='javascript:native.authTaobaoFail()';</script>";
+        echo "<script type='text/javascript'> alert('授权失败 ".$msg."'); window.location.href='NativeInterface.authTaobaoFail';</script>";
         exit;
 
     }
